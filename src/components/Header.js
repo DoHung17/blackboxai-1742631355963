9cb388b8fbddbbd0e6351956892e1d0b8420afb3
DoHung17 <hunghungg1309@gmail.com>
2025-03-22 15:16:21 +0700

    Add src/components/Header.js 1742631380689

---
+++ src/components/Header.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  useTheme,
+  useMediaQuery
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = [
+    { text: 'Home', path: '/' },
+    { text: 'Latest', path: '/blog/latest' },
+    { text: 'Popular', path: '/blog/popular' },
+    { text: 'About', path: '/about' }
+  ];
+
+  return (
+    <AppBar position="sticky" color="default" elevation={1}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            component={RouterLink}
+            to="/"
+            sx={{
+              flexGrow: 1,
+              textDecoration: 'none',
+              color: 'primary.main',
+              fontWeight: 700,
+              letterSpacing: 0.5
+            }}
+          >
+            MUI Blog
+          </Typography>
+
+          {isMobile ? (
+            <>
+              <IconButton
+                edge="end"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                {menuItems.map((item) => (
+                  <MenuItem
+                    key={item.text}
+                    component={RouterLink}
+                    to={item.path}
+                    onClick={handleClose}
+                  >
+                    {item.text}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </>
+          ) : (
+            <Box sx={{ display: 'flex', gap: 2 }}>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  component={RouterLink}
+                  to={item.path}
+                  color="inherit"
+                  sx={{
+                    '&:hover': {
+                      color: 'primary.main',
+                    },
+                  }}
+                >
+                  {item.text}
+                </Button>
+              ))}
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Header;
